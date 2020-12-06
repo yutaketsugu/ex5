a0e326af85ce65ac3253528ac0d5180de64a55e0
武次優 <72009330+yutaketsugu@users.noreply.github.com>
2020-12-06 22:31:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
 function randomValueFromArray(array){
-    const random = Math.floor(math.random()*array.length);
+    const random = Math.floor(Math.random()*array.length);
     return array[random];
 }
 
